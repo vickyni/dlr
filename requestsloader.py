3454ef012b68dba6719425b4da0c3b6bbcaaee9c
simonhlm <455765506@qq.com>
2018-01-30 17:20:42 +0800

    init the unit test module

--- requestsloader.py
+++ requestsloader.py
@@ -21,7 +21,14 @@ class RequestsLoader(object):
 	"""docstring for ParameterLoader
 	load the parameter file and retrun the list of request
 	"""
-	def __init__(self, parameter_filename=PARMFILE_NAME, sheetname=DEF_SHEET_NAME):
+	def __init__(self):
+		self.parameter_filename = ''
+		self.sheetname = ''
+		self.wb = None
+		self.sheet = None
+
+	def load_workbook(self, parameter_filename=PARMFILE_NAME, sheetname=DEF_SHEET_NAME):
+
 		self.parameter_filename = parameter_filename
 		try:
 			self.wb = load_workbook(self.parameter_filename)
@@ -29,17 +36,27 @@ class RequestsLoader(object):
 			raise
 
 		try:
-			self.sheet = self.wb.get_sheet_by_name(sheetname)
+			self.sheet = self.wb[sheetname]
 		except Exception as e:
 			raise
 
 	def get_records(self):
+
+		if self.sheet is None:
+			logging.error('Please init the loadrequest object')
+			return None
+
 		records = []
 		for row in range(1, self.sheet.max_row+1):
 			records.append(self.sheet[row])
 		return records
 
 	def get_requests(self):
+
+		if self.sheet is None:
+			logging.error('Please init the loadrequest object')
+			return None
+
 		records_list = []
 		title_value = [col.value for col in self.sheet[1]]
 
@@ -51,6 +68,11 @@ class RequestsLoader(object):
 		return records_list
 
 	def get_requests_str(self):
+
+		if self.sheet is None:
+			logging.error('Please init the loadrequest object')
+			return None
+
 		records_list = []
 		title_value = [col.value for col in self.sheet[1]]
 
@@ -68,7 +90,8 @@ class RequestsLoader(object):
 
 
 if __name__ == '__main__':
-	loadrequest = RequestsLoader(PARMFILE_NAME)
+	loadrequest = RequestsLoader()
+	loadrequest.load_workbook()
 	requests = loadrequest.get_requests_str()
 
 	for request in requests:
---
+++ tests/test_requests_loader.py
@@ -0,0 +1,21 @@
+import unittest
+import sys, os, time
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from requestsloader import RequestsLoader
+
+class TestRquestsLoader(unittest.TestCase):
+	"""docstring for TestRquestsLoader"""
+
+	def setUp(self):
+		self.loader = RequestsLoader()
+
+	def test_not_exist_file(self):
+		with self.assertRaises(FileNotFoundError):
+			self.loader.load_workbook('not_exist_file')
+
+	def tearDown(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
