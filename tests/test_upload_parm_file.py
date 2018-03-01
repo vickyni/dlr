3180cf2a2b3d65d03984946b59f7880866965f96
vickyni <33478393+vickyni@users.noreply.github.com>
2018-03-01 16:42:43 +0800

    Unit test file updated version 0301
    Use Python Unit test module assert mechanism.

--- tests/test_upload_parm_file.py
+++ tests/test_upload_parm_file.py
@@ -16,35 +16,28 @@ class TestUploadParmFile(unittest.TestCase):
         sys.path.append(os.path.dirname(os.getcwd()))
         with self.assertRaises(FileNotFoundError):
             self.loader.load_workbook('C:/Users/IBM_ADMIN/Desktop/LearnClass/Python/dlr-master/dlr.xlsx')
-            print('\nParm File  exists.\nPlease go ahead.\n')
-        print('\nParm File does not exist! \nPlease check...\n')
+              
     
 #test whether there is any record in Parm File
 
     def test_no_request_record(self):
         self.loader.load_workbook('C:/Users/IBM_ADMIN/Desktop/LearnClass/Python/dlr-master/dlr.xlsx')
-        if self.loader.get_records() == None:
-            print('\nNo request record in Parm file.\n')
-        else:
-            print('\nThere are some request records in Parm file.\n')
+        self.assertTrue(self.loader.get_records())
 
 #test function get_requests()
 
     def test_function_get_requests(self):
         self.loader.load_workbook('C:/Users/IBM_ADMIN/Desktop/LearnClass/Python/dlr-master/dlr.xlsx')
-        if self.loader.get_requests() == None:
-            print('No request error!')
-        else:
-            print('\nRequests listed as below: \n', self.loader.get_requests(), '\n')
+        self.assertIsNotNone(self.loader.get_requests())
+
 
 #test function get_requests_str()
 
     def test_function_get_requests_str(self):
         self.loader.load_workbook('C:/Users/IBM_ADMIN/Desktop/LearnClass/Python/dlr-master/dlr.xlsx')
-        if self.loader.get_requests_str() == None:
-            print('No request error!')
-        else:
-            print('Requests list as below: \n', self.loader.get_requests_str(), '\n')
+        l = len(self.loader.get_requests_str())
+        self.assertNotEqual(0,l)
+
 
 
     def tearDown(self):
